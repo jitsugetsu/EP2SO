a217ff5e1b2882aa9d2257eaf42e2b6eabf8a0ef
Gabriel Kendy <lenkiro0akase@gmail.com>
2019-12-01 21:48:22 +0000

    Tentei fazer a classe readerWriter com algum comportamento e com que o main o use.

--- EP2SO/src/ReaderWriter.java
+++ EP2SO/src/ReaderWriter.java
@@ -1,7 +1,15 @@
 public class ReaderWriter{
     int tipo;       //0 = Reader;   1 = Writer
+    String readerVariavel;
     public ReaderWriter(int tipo){
         this.tipo = tipo;
     }
 
+    public void readerCopy(int pos, String[] texto){
+        this.readerVariavel = texto[pos];
+    }
+
+    public void writerAlter(int pos, String [] texto){
+        texto[pos] = "MODIFICADO";
+    }
 }
--- EP2SO/src/main.java
+++ EP2SO/src/main.java
@@ -2,16 +2,29 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Random;
+
 public class main {
+
+	
 	static String [] texto = new String [36242];
+	static ReaderWriter [] writhers = new ReaderWriter [100];
 	public static void main(String[] args) throws FileNotFoundException{
 		File dir = new File("bd.txt");
 		Scanner scan = new Scanner(dir);
 		for(int i = 0; i < 36242; i++){
 			texto[i] = scan.next();
 		}
+
+		Random rand = new Random();
 		
-		
+		for(int i = 0; i < 100; i++){
+			writhers [i] = new ReaderWriter(0);
+			writhers [i].readerCopy(rand.nextInt(36242), texto);
+		}
+		System.out.println(writhers[0].readerVariavel);
+
 	}
 
 }
+
