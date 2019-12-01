a9e355f3fc2c1bead31ad739285ba64b0ee4a501
Gabriel Kendy <lenkiro0akase@gmail.com>
2019-12-01 20:42:38 +0000

    Fiz o arranjo de String

--- EP2SO/src/main.java
+++ EP2SO/src/main.java
@@ -1,9 +1,17 @@
-
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 public class main {
-
-	public static void main(String[] args) {
+	static String [] texto = new String [36242];
+	public static void main(String[] args) throws FileNotFoundException{
+		File dir = new File("bd.txt");
+		Scanner scan = new Scanner(dir);
+		for(int i = 0; i < 36242; i++){
+			texto[i] = scan.next();
+		}
 		
-     System.out.println("teste\nKendy was here");
+		
 	}
 
 }
