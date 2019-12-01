8c69c58ff1336dcc1e865e99e8223f1e090eaa7c
jitsugetsu <raul.rd.douglas@gmail.com>
2019-12-01 17:30:11 -0300

    Update main.java

--- EP2SO/src/main.java
+++ EP2SO/src/main.java
@@ -1,9 +1,19 @@
 
 public class main {
 
-	public static void main(String[] args) {
-		
-     System.out.println("teste\nKendy was here");
-	}
+
+	public static void main(String[] args) throws FileNotFoundException {
+		String[] memo = new String[36242];
+	    int i = 0;
+	    
+	    Scanner scanner = new Scanner(new FileReader("bd.txt")).useDelimiter("\n");
+        while (scanner.hasNext()){
+     	  
+        memo[i] = scanner.next();
+        i++;
+        }
+       System.out.println(memo);
+       scanner.close();
+}
 
 }
